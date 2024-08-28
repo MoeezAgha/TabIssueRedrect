2fedbcec940ed3ab4ea7702ba116884eb79bd1d8
Moeez Agha <aghamoeez@gmail.com>
2024-08-28 17:22:33 -0400

    Add project files.

---
+++ TabIssueRedrect/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TabIssueRedrect
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ TabIssueRedrect/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace TabIssueRedrect
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
